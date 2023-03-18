fa56d5e8d0f45ad96773dbec8e2af62a9fbd0ef2
vartuom <v.artuom@ya.ru>
2023-03-18 19:32:06 +0800

    cors bs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         cors: {
-            origin: "http://localhost:8081",
+            origin: "http://vartuom.nomoredomains.work/",
             credentials: true,
         },
     });
